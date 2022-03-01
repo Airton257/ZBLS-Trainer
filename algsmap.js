7f02ae710b9ac6f6bc0ee99b329a9e24048e5f02
Airton257 <100723566+Airton257@users.noreply.github.com>
2022-03-01 16:57:09 -0300

    Create algsmap.js

---
+++ algsmap.js
@@ -0,0 +1,24 @@
+﻿var zblsMap = {
+  1: [
+    "R' U' F R' F' R2 U R B' R' B",
+    "L U F' L F L2 U' L' B L B'",
+    "L U2 L2 B L B' U2 L' B L B'",
+    "R' U2 R2 B' R' B U2 R B' R' B",
+    "B U' B2 D' F R' F' D B2 U B'",
+    "L' B2 R2 B R' B L U2 B' R' B",
+    "F' U F2 D B' R B D' F2 U' F",
+    "R B2 L2 B' L B' R' U2 B L B'",
+    "L R' F R U2 F' U2 F' U2 F U2 L'",
+    "L R' F' L' U2 F U2 F U2 F' U2 R",
+    "L U F' L F L2 B' U' B L U' L'",
+    "R' U' F R' F' R2 B U B' R' U R",
+    "R' U' F R' F' R2 B U' L U L' B'",
+    "L U F' L F L2 B' U R' U' R B",
+    "L U F' L F L2 B2 R B R' U' B",
+    "R' U' F R' F' R2 B2 L' B' L U B'",
+    "R2 B2 L' B R' B2 L2 U L' U B' R'",
+    "L2 B2 R B' L B2 R2 U' R U' B L",
+    "L R' F R U2 B' R2 F' R2 B U2 L'",
+    "L R' F' L' U2 B L2 F L2 B' U2 R",
+  ]
+  };
